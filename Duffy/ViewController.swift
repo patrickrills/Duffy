c8210965b575b68da29b7de3b8cdaab573f75e51
Patrick Rills <rills@loseit.com>
2016-06-28 10:39:21 -0400

    Initial Commit

---
+++ Duffy/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Duffy
+//
+//  Created by Patrick Rills on 6/28/16.
+//  Copyright © 2016 Big Blue Fly. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
