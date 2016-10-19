c0a530e32311c24de24138b89a35db33790b6a58
Patrick Rills <patrickrills@Patricks-MacBook-Pro.local>
2016-10-18 21:42:38 -0400

    Put starting of observer queries back in watch app launch

--- Duffy WatchKit Extension/ExtensionDelegate.swift
+++ Duffy WatchKit Extension/ExtensionDelegate.swift
@@ -22,12 +22,12 @@ class ExtensionDelegate: NSObject, WKExtensionDelegate, WCSessionServiceDelegate
     
     func applicationDidFinishLaunching() {
         // Perform any final initialization of your application.
-        
+        HealthKitService.getInstance().initializeBackgroundQueries()
     }
 
     func applicationDidBecomeActive() {
         // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
-        HealthKitService.getInstance().initializeBackgroundQueries()
+        
     }
 
     func applicationWillResignActive() {
