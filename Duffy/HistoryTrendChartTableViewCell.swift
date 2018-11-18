12df14af382ac3ff15c26f4f4b0542f395f32e56
Patrick Rills <bigbluefly@hotmail.com>
2018-11-18 17:25:16 -0500

    Begin to rearrange History screen to include a chart

---
+++ Duffy/HistoryTrendChartTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  HistoryTrendChartTableViewCell.swift
+//  Duffy
+//
+//  Created by Patrick Rills on 11/18/18.
+//  Copyright © 2018 Big Blue Fly. All rights reserved.
+//
+
+import UIKit
+
+class HistoryTrendChartTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
