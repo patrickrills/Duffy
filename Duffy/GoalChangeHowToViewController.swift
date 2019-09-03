94216e4f2f021e17ae86d755a8b7106b73da9b67
Patrick Rills <bigbluefly@hotmail.com>
2019-09-02 20:33:19 -0400

    Finalize instructions text and add images

--- Duffy/GoalChangeHowToViewController.swift
+++ Duffy/GoalChangeHowToViewController.swift
@@ -7,9 +7,15 @@
 //
 
 import UIKit
+import DuffyFramework
 
 class GoalChangeHowToViewController: UIViewController {
 
+    @IBOutlet private var headerLabel: UILabel!
+    @IBOutlet private var oneLabel: UILabel!
+    @IBOutlet private var twoLabel: UILabel!
+    @IBOutlet private var threeLabel: UILabel!
+    
     init() {
         super.init(nibName: "GoalChangeHowToViewController", bundle: nil)
     }
@@ -21,6 +27,11 @@ class GoalChangeHowToViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        self.title = "How To Change Goal"
+        self.title = "Change Your Goal"
+        self.headerLabel.textColor = .darkGray
+        self.headerLabel.text = String(format: "This guide describes how to change your daily steps goal (currently %@ steps). Your goal can only be changed from the Duffy Apple Watch app.", Globals.stepsFormatter().string(from: NSNumber(value: HealthCache.getStepsDailyGoal()))!)
+        self.oneLabel.textColor = Globals.primaryColor()
+        self.twoLabel.textColor = self.oneLabel.textColor
+        self.threeLabel.textColor = self.oneLabel.textColor
     }
 }
