793ab0ed57e6af56c5cd9b3c7e158b46056bd026
Patrick Rills <rills@loseit.com>
2019-09-15 10:56:02 -0400

    Modernize Week controller

--- Duffy WatchKit Extension/WeekInterfaceController.swift
+++ Duffy WatchKit Extension/WeekInterfaceController.swift
@@ -13,73 +13,98 @@ import DuffyWatchFramework
 
 class WeekInterfaceController: WKInterfaceController
 {
-    @IBOutlet weak var stepsTable: WKInterfaceTable?
+    @IBOutlet weak var stepsTable: WKInterfaceTable!
     
     override func didAppear()
     {
         super.didAppear()
         
-        bindTableToWeek()
+        retrieveRecentSteps()
     }
 
-    func bindTableToWeek()
+    private func retrieveRecentSteps()
     {
-        let startDate = (Calendar.current as NSCalendar).date(byAdding: .day, value: -14, to: Date(), options: NSCalendar.Options(rawValue: 0))
+        guard let startDate = Calendar.current.date(byAdding: .day, value: -14, to: Date()) else {
+            showErrorState()
+            return
+        }
         
-        HealthKitService.getInstance().getSteps(startDate!, toEndDate: Date(), onRetrieve: {
+        HealthKitService.getInstance().getSteps(startDate, toEndDate: Date(), onRetrieve: {
             (stepsCollection: [Date : Int]) in
             
-            DispatchQueue.main.async {
-                [weak self] in
-                if let weakSelf = self
-                {
-                    let sortedKeys = stepsCollection.keys.sorted(by: {
-                        (date1: Date, date2: Date) in
-                        return date1.timeIntervalSince1970 > date2.timeIntervalSince1970
-                    })
+                DispatchQueue.main.async {
+                    [weak self] in
+                    if let weakSelf = self {
+                        let sortedKeys = stepsCollection.keys.sorted(by: {
+                            (date1: Date, date2: Date) in
+                            return date1.timeIntervalSince1970 > date2.timeIntervalSince1970
+                        })
                     
-                    if (sortedKeys.count == 0) { return }
+                        if (sortedKeys.count == 0) {
+                            weakSelf.showErrorState()
+                            return
+                        }
                     
-                    var rowTypes = [String]()
-                    for _ in 1...sortedKeys.count
-                    {
-                        rowTypes.append("WeekRowController")
-                    }
+                        let dateFormatter = DateFormatter()
+                        dateFormatter.dateFormat = "eee"
                     
-                    weakSelf.stepsTable?.setRowTypes(rowTypes)
+                        let numFormatter = NumberFormatter()
+                        numFormatter.numberStyle = .decimal
+                        numFormatter.locale = Locale.current
                     
-                    var idx = 0
-                    
-                    let dateFormatter = DateFormatter()
-                    dateFormatter.dateFormat = "eee"
-                    
-                    let numFormatter = NumberFormatter()
-                    numFormatter.numberStyle = .decimal
-                    numFormatter.locale = Locale.current
-                    
-                    for key in sortedKeys
-                    {
-                        let stepRow = weakSelf.stepsTable?.rowController(at: idx) as! WeekRowController
-                        stepRow.dateLabel?.setText(dateFormatter.string(from: key).uppercased())
-                        stepRow.stepsLabel?.setText("0")
+                        var data = [WeekRowData]()
                         
-                        if let steps = stepsCollection[key]
-                        {
-                            stepRow.stepsLabel?.setText(numFormatter.string(from: NSNumber(value: steps)))
-                            stepRow.adornmentLabel?.setText(HealthKitService.getInstance().getAdornment(for: steps))
+                        for key in sortedKeys {
+                            let title = dateFormatter.string(from: key).uppercased()
+                            var value = "0"
+                            var adornment = ""
+                            
+                            if let steps = stepsCollection[key],
+                                let formattedSteps = numFormatter.string(from: NSNumber(value: steps)) {
+                                
+                                value = formattedSteps
+                                adornment = HealthKitService.getInstance().getAdornment(for: steps)
+                            }
+                            
+                            data.append(WeekRowData(title: title, formattedValue: value, adornment: adornment))
                         }
                         
-                        idx += 1
+                        weakSelf.bindTable(to: data)
                     }
-                }
                 }
             },
             onFailure: {
-                (err: Error?) in
+                [weak self] (err: Error?) in
                 DispatchQueue.main.async {
-                    print("error: \(err?.localizedDescription ?? "no error")")
+                    self?.showErrorState()
                 }
         })
     }
     
+    private func bindTable(to data: [WeekRowData]) {
+        var rowTypes = [String]()
+        for _ in 1...data.count
+        {
+            rowTypes.append("WeekRowController")
+        }
+        
+        stepsTable.setRowTypes(rowTypes)
+        
+        for (index, row) in data.enumerated() {
+            let stepRow = stepsTable.rowController(at: index) as! WeekRowController
+            stepRow.dateLabel?.setText(row.title)
+            stepRow.stepsLabel?.setText(row.formattedValue)
+            stepRow.adornmentLabel?.setText(row.adornment)
+        }
+    }
+    
+    private func showErrorState() {
+        bindTable(to: [WeekRowData(title: "No Data", formattedValue: "", adornment: "")])
+    }
+    
+    struct WeekRowData {
+        var title: String
+        var formattedValue: String
+        var adornment: String
+    }
 }
