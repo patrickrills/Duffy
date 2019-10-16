3b14cd4cf87f332989e526a9239f95cabc69c8f9
Patrick Rills <bigbluefly@hotmail.com>
2019-10-15 21:10:16 -0400

    Fix attributed string of daily average

--- Duffy/HistoryTrendChartTableViewCell.swift
+++ Duffy/HistoryTrendChartTableViewCell.swift
@@ -13,27 +13,26 @@ class HistoryTrendChartTableViewCell: UITableViewCell
     @IBOutlet weak var averageLabel : UILabel?
     @IBOutlet weak var chart : HistoryTrendChartView?
     
-    override func awakeFromNib()
-    {
+    override func awakeFromNib() {
         super.awakeFromNib()
         selectionStyle = .none
     }
     
-    func bind(toStepsByDay: [Date : Int])
-    {
+    func bind(toStepsByDay: [Date : Int]) {
         var average : Int = 0
         
-        if toStepsByDay.count > 0
-        {
+        if toStepsByDay.count > 0 {
             average = toStepsByDay.values.reduce(0, +) / toStepsByDay.count
         }
         
         let averageFormatted = Globals.stepsFormatter().string(from: NSNumber(value: average))!
         let averageAttributed = NSMutableAttributedString(string: String(format: NSLocalizedString("%@ daily average", comment: ""), averageFormatted))
-        averageAttributed.addAttribute(.font, value: UIFont.systemFont(ofSize: 24.0, weight: .medium), range: NSRange(location: 0, length: averageFormatted.count))
-        averageAttributed.addAttribute(.font, value: UIFont.systemFont(ofSize: 15.0), range: NSRange(location: averageFormatted.count, length: averageAttributed.length - averageFormatted.count))
-        averageLabel?.attributedText = averageAttributed
         
+        if let numberRange = averageAttributed.string.range(of: averageFormatted) {
+            averageAttributed.addAttribute(.font, value: UIFont.systemFont(ofSize: 24.0, weight: .medium), range: NSRange(numberRange, in: averageAttributed.string))
+        }
+        
+        averageLabel?.attributedText = averageAttributed
         chart?.dataSet = toStepsByDay
     }
 }
