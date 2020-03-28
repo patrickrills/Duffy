c0412332c5dae44bd035a161fe25dff2c3e03ce1
Patrick Rills <bigbluefly@hotmail.com>
2020-03-28 17:14:02 -0400

    Add empty ring guide to today

--- DuffyToday/ProgressRingView.swift
+++ DuffyToday/ProgressRingView.swift
@@ -17,7 +17,7 @@ class ProgressRingView: UIView {
     }
 
     private let START_ANGLE: CGFloat = 270.0
-    private let INSET: CGFloat = 4.0
+    private let PROGRESS_WIDTH: CGFloat = 4.0
     
     override func draw(_ rect: CGRect) {
         super.draw(rect)
@@ -28,15 +28,31 @@ class ProgressRingView: UIView {
                 return
         }
         
-        let insetRect = rect.inset(by: UIEdgeInsets(top: INSET, left: INSET, bottom: INSET, right: INSET))
+        let insetRect = rect.inset(by: UIEdgeInsets(top: PROGRESS_WIDTH, left: PROGRESS_WIDTH, bottom: PROGRESS_WIDTH, right: PROGRESS_WIDTH))
+        let center = CGPoint(x: rect.size.width / 2.0, y: rect.size.height / 2.0)
+        let radius = insetRect.size.width / 2.0
+        
+        if progress < 1.0 {
+            let emptyRing = UIBezierPath(arcCenter: center, radius: radius, startAngle: 0, endAngle: .pi * 2, clockwise: true)
+            emptyRing.lineWidth = 1.0
+            if #available(iOS 13.0, *) {
+                UIColor.quaternaryLabel.withAlphaComponent(0.1).setStroke()
+            } else {
+                UIColor.lightGray.withAlphaComponent(0.25).setStroke()
+            }
+            emptyRing.stroke()
+            UIColor.clear.setFill()
+            emptyRing.fill()
+        }
+        
         let startAngle: CGFloat = (START_ANGLE / 360.0 + START_ANGLE) * .pi / 180
         let endAngle: CGFloat = ((progress * 360.0) + START_ANGLE)  * .pi / 180
-        let ring = UIBezierPath(arcCenter: CGPoint(x: rect.size.width / 2.0, y: rect.size.height / 2.0), radius: insetRect.size.width / 2.0, startAngle: startAngle, endAngle: endAngle, clockwise: true)
-        ring.lineWidth = 4.0
+        let ring = UIBezierPath(arcCenter: center, radius: radius, startAngle: startAngle, endAngle: endAngle, clockwise: true)
+        ring.lineWidth = PROGRESS_WIDTH
         if progress >= 1.0 {
-            successColor.setStroke()
+            successColor.withAlphaComponent(0.85).setStroke()
         } else {
-            ringColor.setStroke()
+            ringColor.withAlphaComponent(0.85).setStroke()
         }
         ring.stroke()
         UIColor.clear.setFill()
