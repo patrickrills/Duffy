da2c3bb4b9b76178c07687487609f79abbfc5846
Patrick Rills <bigbluefly@hotmail.com>
2020-08-08 13:09:07 -0400

    Reorganize graph drawing

--- Duffy/HistoryTrendChartView.swift
+++ Duffy/HistoryTrendChartView.swift
@@ -16,10 +16,20 @@ class HistoryTrendChartView: UIView
             setNeedsDisplay()
         }
     }
-
+    
+    typealias Plot = (points: [CGPoint], goalY: CGFloat, averageY: CGFloat?)
+    
     override func draw(_ rect: CGRect) {
+        let graphPlot = plot(in: rect)
+        drawDataLine(with: graphPlot, in: rect)
+        drawGoalLine(with: graphPlot, in: rect)
+    }
+    
+    private func plot(in rect: CGRect) -> Plot {
         let goalSteps = HealthCache.getStepsDailyGoal()
-        var goalLineY = rect.size.height / 2.0
+        var goalLineY: CGFloat = rect.size.height / 2.0
+        let averageY: CGFloat? = nil
+        var points = [CGPoint]()
         
         if (dataSet.count > 0) {
             let maxSteps = dataSet.values.max()
@@ -27,44 +37,58 @@ class HistoryTrendChartView: UIView
             let widthOfDay = Double(rect.width) / Double(dataSet.count)
             goalLineY = rect.size.height - CGFloat(floor((Double(goalSteps) / topRange) * Double(rect.size.height)))
             
+            for (index, day) in dataSet.keys.sorted().enumerated() {
+                let stepsForDay = dataSet[day]!
+                let dayY = Double(rect.size.height) - (Double(stepsForDay) / topRange) * Double(rect.size.height)
+                let dayX = widthOfDay * Double(index) + floor(widthOfDay * 0.5)
+                points.append(CGPoint(x: dayX, y: dayY))
+            }
+        }
+        
+        return Plot(points: points, goalY: goalLineY, averageY: averageY)
+    }
+    
+    private func drawDataLine(with plot: Plot, in rect: CGRect) {
+        
+        //TODO: check to see if data line option is enabled
+        
+        if plot.points.count > 0 {
             let dataLine = UIBezierPath()
-            
-            if (widthOfDay <= 0.5) {
+            let xWidth = rect.width / CGFloat(plot.points.count)
+    
+            if (xWidth <= 0.5) {
                 dataLine.lineWidth = 0.5
-            } else if (widthOfDay < 2.0) {
+            } else if (xWidth < 2.0) {
                 dataLine.lineWidth = 1.0
             } else {
                 dataLine.lineWidth = 2.0
             }
             
-            for (index, day) in dataSet.keys.sorted().enumerated() {
-                let stepsForDay = dataSet[day]!
-                let dayY = Double(rect.size.height) - (Double(stepsForDay) / topRange) * Double(rect.size.height)
-                let dayX = widthOfDay * Double(index) + floor(widthOfDay * 0.5)
-                let dayPoint = CGPoint(x: dayX, y: dayY)
-                
-                if index == 0 {
-                    dataLine.move(to: dayPoint)
+            plot.points.forEach({
+                if dataLine.isEmpty {
+                    dataLine.move(to: $0)
                 } else {
-                    dataLine.addLine(to: dayPoint)
+                    dataLine.addLine(to: $0)
                 }
-            }
+            })
             
             Globals.primaryColor().setStroke()
             dataLine.stroke()
         }
-        
+    }
+    
+    private func drawGoalLine(with plot: Plot, in rect: CGRect) {
         let shoe = NSAttributedString(string: Trophy.shoe.symbol(), attributes: [.font : UIFont.systemFont(ofSize: UIFont.systemFontSize)])
         let shoeSize = shoe.size()
         
         let dotted = UIBezierPath()
-        dotted.move(to: CGPoint(x: shoeSize.width + 1, y: goalLineY))
-        dotted.addLine(to: CGPoint(x: rect.size.width, y: goalLineY))
+        dotted.move(to: CGPoint(x: shoeSize.width + 1, y: plot.goalY))
+        dotted.addLine(to: CGPoint(x: rect.size.width, y: plot.goalY))
         dotted.lineWidth = 1.0
         dotted.setLineDash([2.0, 2.0], count: 2, phase: 0.0)
         Globals.lightGrayColor().setStroke()
         dotted.stroke()
         
-        shoe.draw(at: CGPoint(x: 0, y: goalLineY - (shoeSize.height / 2.0)))
+        shoe.draw(at: CGPoint(x: 0, y: plot.goalY - (shoeSize.height / 2.0)))
     }
 }
