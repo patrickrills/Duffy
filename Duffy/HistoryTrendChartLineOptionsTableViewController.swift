309ccc102dc711af0fcf970d951ce80380391cc4
Patrick Rills <bigbluefly@hotmail.com>
2020-09-07 14:09:57 -0400

    Reorganization chart line options so user can only pick valid states

---
+++ Duffy/HistoryTrendChartLineOptionsTableViewController.swift
@@ -0,0 +1,59 @@
+//
+//  HistoryTrendChartLineOptionsTableViewController.swift
+//  Duffy
+//
+//  Created by Patrick Rills on 9/7/20.
+//  Copyright © 2020 Big Blue Fly. All rights reserved.
+//
+
+import UIKit
+
+class HistoryTrendChartLineOptionsTableViewController: UITableViewController {
+
+    private let lineOptions: [HistoryTrendChartLineOption] = HistoryTrendChartLineOption.allCases
+    
+    init() {
+        super.init(style: .grouped)
+        
+        title = "Show Lines" //TODO: Translate to Japanese
+        
+        if #available(iOS 13.0, *) {
+            navigationItem.rightBarButtonItem = UIBarButtonItem(image: UIImage(systemName: "checkmark.circle"), style: .plain, target: self, action: #selector(navigateBack))
+        } else {
+            navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .done, target: self, action: #selector(navigateBack))
+        }
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("xib is not used for HistoryTrendChartOptionsTableViewController")
+    }
+    
+    @objc private func navigateBack() {
+        navigationController?.popViewController(animated: true)
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        lineOptions.count
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: .default, reuseIdentifier: nil)
+        let option = lineOptions[indexPath.row]
+        cell.textLabel?.text = option.displayName()
+        cell.accessoryType = option.isEnabled() ? .checkmark : .none
+        cell.tintColor = Globals.primaryColor()
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let option = lineOptions[indexPath.row]
+        option.setEnabled()
+        tableView.reloadData()
+    }
+}
--- Duffy/HistoryTrendChartOption.swift
+++ Duffy/HistoryTrendChartOption.swift
@@ -17,13 +17,13 @@ enum HistoryTrendChartOption: String, CaseIterable {
     func displayName() -> String { //TODO: Japanese translations
         switch self {
         case .actualDataLine:
-            return "Data Line"
+            return "Detail"
         case .trendLine:
-            return "Trend Line"
+            return "Trend"
         case .averageIndicator:
-            return "Average Indicator"
+            return "Average"
         case .goalIndicator:
-            return "Goal Indicator"
+            return "Goal"
         }
     }
     
@@ -34,5 +34,56 @@ enum HistoryTrendChartOption: String, CaseIterable {
     
     func setEnabled(_ enabled: Bool) {
         UserDefaults.standard.set(enabled, forKey: self.rawValue)
+    }
+}
+
+enum HistoryTrendChartLineOption: CaseIterable {
+    case actual, trend, both
+    
+    func displayName() -> String { //TODO: Japanese translations
+        switch self {
+        case .actual:
+            return HistoryTrendChartOption.actualDataLine.displayName()
+        case .trend:
+            return HistoryTrendChartOption.trendLine.displayName()
+        case .both:
+            return String(format: "%@ & %@", HistoryTrendChartLineOption.actual.displayName(), HistoryTrendChartLineOption.trend.displayName())
+        }
+    }
+    
+    func isEnabled() -> Bool {
+        return self == HistoryTrendChartLineOption.currentValue()
+    }
+    
+    func setEnabled() {
+        guard !isEnabled() else { return }
+        
+        var actualEnabled = false
+        var trendEnabled = false
+        
+        switch self {
+        case .actual:
+            actualEnabled = true
+        case .trend:
+            trendEnabled = true
+        case .both:
+            actualEnabled = true
+            trendEnabled = true
+        }
+        
+        HistoryTrendChartOption.actualDataLine.setEnabled(actualEnabled)
+        HistoryTrendChartOption.trendLine.setEnabled(trendEnabled)
+    }
+    
+    static func currentValue() -> HistoryTrendChartLineOption {
+        switch (HistoryTrendChartOption.actualDataLine.isEnabled(), HistoryTrendChartOption.trendLine.isEnabled()) {
+        case (true, false),
+             (false, false):
+            return .actual
+        case (false, true):
+            return .trend
+        case (true, true):
+            return .both
+        }
     }
 }
