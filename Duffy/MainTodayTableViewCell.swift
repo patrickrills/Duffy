d1f63d0b9d9ae8c6b7c4c25ba0fba696f625c1f8
Patrick Rills <bigbluefly@hotmail.com>
2021-03-20 11:25:50 -0400

    Stub out xib for new today cell

---
+++ Duffy/MainTodayTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  MainTodayTableViewCell.swift
+//  Duffy
+//
+//  Created by Patrick Rills on 3/20/21.
+//  Copyright © 2021 Big Blue Fly. All rights reserved.
+//
+
+import UIKit
+
+class MainTodayTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
