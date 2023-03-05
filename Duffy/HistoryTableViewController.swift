78dd02d1c887fe729a6d3150e7fc9351ac69447a
Patrick Rills <bigbluefly@hotmail.com>
2023-03-05 10:11:37 -0500

    Move DetailSortOption into its own file

--- Duffy/HistoryTableViewController.swift
+++ Duffy/HistoryTableViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 import DuffyFramework
 
 class HistoryTableViewController: UITableViewController {
+    
+    //MARK: Constants and Definitions
+    
     private enum Constants {
         static let ROW_HEIGHT: CGFloat = PreviousValueTableViewCell.rowHeight
         static let PAGE_SIZE_DAYS: Int = 30
@@ -22,53 +25,7 @@ class HistoryTableViewController: UITableViewController {
         case chart, summary, details
     }
     
-    private enum DetailSortOption: String, CaseIterable {
-        case newestToOldest = "newestToOldest"
-        case oldestToNewest = "oldestToNewest"
-        
-        static func sort(option: DetailSortOption, dates: inout [Date]) {
-            dates.sort(by: { sort(option: option, date1: $0, date2: $1) })
-        }
-        
-        private static func sort(option: DetailSortOption, date1: Date, date2: Date) -> Bool {
-            switch option {
-            case .newestToOldest:
-                return date1 > date2
-            case .oldestToNewest:
-                return date1 < date2
-            }
-        }
-        
-        func displayText() -> NSAttributedString {
-            let attributedText = NSMutableAttributedString(string: String(format: "%@ ", NSLocalizedString("Sort", comment: "")))
-            let symbolName = self.symbolName()
-            let symbolConfiguration = UIImage.SymbolConfiguration(font: UIFont.preferredFont(forTextStyle: .body))
-            let symbolImage = UIImage(systemName: symbolName, withConfiguration: symbolConfiguration)?.withRenderingMode(.alwaysTemplate)
-            let symbolTextAttachment = NSTextAttachment()
-            symbolTextAttachment.image = symbolImage
-            let attachmentString = NSMutableAttributedString(attachment: symbolTextAttachment)
-            attributedText.append(attachmentString)
-            return attributedText
-        }
-        
-        func menuOptionText() -> String {
-            switch self {
-            case .newestToOldest:
-                return NSLocalizedString("Newest to Oldest", comment: "")
-            case .oldestToNewest:
-                return NSLocalizedString("Oldest to Newest", comment: "")
-            }
-        }
-        
-        func symbolName() -> String {
-            switch self {
-            case .newestToOldest:
-                return "arrow.down"
-            case .oldestToNewest:
-                return "arrow.up"
-            }
-        }
-    }
+    //MARK: Properties and State
     
     private let goal = HealthCache.dailyGoal()
     
